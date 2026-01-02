fbf04523ca5cadfee4d32afbce082abe3cc00be3
patgar-suraj <surajrp7012@gmail.com>
2026-01-02 13:15:12 +0530

    - Added @google/genai and dotenv dependencies to package.json. - Updated server.js to handle AI message events and emit responses. - Created ai.service.js to encapsulate AI response generation logic using Google GenAI.

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require("dotenv").config()
 const app = require("./src/app")
 const {createServer} = require("http")
 const {Server} = require("socket.io")
+const generateResponse = require("./src/service/ai.service")
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
+    console.log("a user connected")
 
+    socket.on("disconnect", ()=>{
+        console.log("a user disconnected")
+    })
+
+    socket.on("ai-message", async(data)=>{
+        console.log("recived ai message:", data.prompt)
+        const response = await generateResponse(data.prompt)
+        console.log("ai response:", response)
+        socket.emit("ai-message-response", {response})
+    })
 })
 
 httpServer.listen(3000, ()=>{
